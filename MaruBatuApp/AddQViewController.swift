8f15176376a02a0b613ac22d6b2bc8e64885162d
katouyo <yothiac13@gmail.com>
2018-10-30 15:33:16 +0900

    課題1完成

---
+++ MaruBatuApp/AddQViewController.swift
@@ -0,0 +1,30 @@
+//
+//  AddQViewController.swift
+//  MaruBatuApp
+//
+//  Created by Yoshiaki Kato on 2018/10/30.
+//  Copyright © 2018 Yoshiaki Kato. All rights reserved.
+//
+
+import UIKit
+
+class AddQViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
